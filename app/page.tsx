19dca168c7b2b3b441ac2e3602189c38b820c072
Madeeha Shakil <madeeha.pafkiet@gmail.com>
2024-11-01 13:02:51 +0500

    Tailwaind CSS Setup & Theming

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-black text-primary-500">
+        Welcome to the world of Next.js
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
